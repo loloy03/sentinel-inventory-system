2a723c4e4832751fd745f843e27dc23ece4f131d
threewhitecats <78993871+loloy03@users.noreply.github.com>
2025-03-12 22:58:45 +0800

    Add login_required decorator to views for access control

--- inventory_system/account/views.py
+++ inventory_system/account/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+# Import the login_required decorator to restrict access to authenticated users
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
