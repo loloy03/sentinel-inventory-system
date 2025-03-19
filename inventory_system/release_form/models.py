cbb182f0e4ed93f52ff40efa63821df380456e43
threewhitecats <78993871+loloy03@users.noreply.github.com>
2025-03-19 15:36:56 +0800

    Add ReleasedStock model to manage released inventory data

--- inventory_system/release_form/models.py
+++ inventory_system/release_form/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class ReleasedStock(models.Model):
+    production_id = models.CharField(max_length=100)
+    date = models.DateField()
+    product = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+    item_code = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    pallet_position = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+    warehouse_id = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.production_id} - {self.product} ({self.color})"
