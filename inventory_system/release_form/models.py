dbb955fd70286e61752192cebe11a232a31d81e7
threewhitecats <78993871+loloy03@users.noreply.github.com>
2025-03-24 18:44:34 +0800

    Update ReleasedStock model and migration to adjust field definitions for production_code and warehouse_id

--- inventory_system/release_form/models.py
+++ inventory_system/release_form/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class ReleasedStock(models.Model):
-    production_code = models.CharField(max_length=100, primary_key=True)
+    warehouse_id = models.CharField(max_length=100, blank=True, primary_key=True)
+    production_code = models.CharField(max_length=100)
     date = models.DateField()
     product = models.CharField(max_length=100)
     color = models.CharField(max_length=50)
@@ -10,7 +11,6 @@ class ReleasedStock(models.Model):
     quantity = models.IntegerField()
     pallet_position = models.CharField(max_length=50)
     status = models.CharField(max_length=50)
-    warehouse_id = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return f"{self.production_code} - {self.product} ({self.color})"
