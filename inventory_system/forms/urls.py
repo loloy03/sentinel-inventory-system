295a527a035976db4039cb9ab14d5ec1c638eb24
threewhitecats <78993871+loloy03@users.noreply.github.com>
2025-03-25 15:46:01 +0800

    Add search key URL path to forms app

--- inventory_system/forms/urls.py
+++ inventory_system/forms/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('release', views.release_form, name='release_form'),
     path('warehouse/outside/', views.warehouse_outside, name='warehouse_outside'),
     path('warehouse/area/', views.warehouse_area, name='warehouse_area'),
+    path('search/key', views.search_key, name='search_key'),
 ]
