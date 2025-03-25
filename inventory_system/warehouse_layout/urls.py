54211cd7c533c3f6c6a6c93567a452e6827b22d2
threewhitecats <78993871+loloy03@users.noreply.github.com>
2025-03-25 19:00:57 +0800

    Remove warehouse layout module and associated files

--- inventory_system/warehouse_layout/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name='warehouse_layout'
-
-urlpatterns = [
-    path('', views.warehouse_map, name='warehouse_map'),
-]
--- inventory_system/warehouse_layout/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def warehouse_map(request):
-    return render(request, 'warehouse_layout/warehouse_layout.html')
